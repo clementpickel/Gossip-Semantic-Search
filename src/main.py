cb6c5264854811349e5b2371611b172d1017a91e
Clément Pickel <clement.pickel@epitech.eu>
2025-03-10 19:47:44 +0100

    Update loading to remove interaction with the code

--- src/main.py
+++ src/main.py
@@ -3,15 +3,19 @@ from src.dto import ArticleDto, ParameterDto
 from src.database import Database
 import numpy as np
 from fastapi import FastAPI, HTTPException
+from pathlib import Path
+
+db_name = 'db'
 
 app = FastAPI()
 database = Database()
 extract = Extract(db=database)
 
-# extract.get_and_save()
-# database.save("db")
-# OR
-database.load("db")
+if Path(f"{db_name}.index").exists() and Path(f"{db_name}.meta").exists():
+    database.load(db_name)
+else:
+    extract.get_and_save()
+    database.save(db_name)
 
 database.print()
 
@@ -22,6 +26,7 @@ database.print()
 def update_db():
     global extract
     extract.get_and_save()
+    database.save()
     return None
 
 @app.post("/api/article/",
